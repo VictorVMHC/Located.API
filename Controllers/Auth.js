f86b90af77b629cea498843ef92d324f9f1fe7f7
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-07-12 21:09:41 -0600

    Added auth endpoint

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -22,7 +22,7 @@ const AuthLogin = async (req = request, res = response) => {
         });
 
         return res.status(200).json({
-            msg: 'User',
+            user,
             token: token
         });
 
@@ -34,6 +34,31 @@ const AuthLogin = async (req = request, res = response) => {
     }
 }
 
+const Auth = async (req = request, res = response) => {
+    const tokenDecoded = req.tokenDecoded
+    try{
+        const user = await User.findById(tokenDecoded.id)
+        if(!user){
+            return res.status(404).json({ error: 'User not found' }); 
+        }
+
+        const token = jwt.sign({id: user._id, email: user.email}, process.env.TOKEN_SECRET,{
+            expiresIn: '30 days'
+        });
+
+        return res.status(200).json({
+            user,
+            token
+        })
+    }catch(err){
+        return res.status(500).json({
+            msg: 'An error occurred while finding the user',
+            emailRequested: tokenDecoded.email,
+        });
+    }
+}
+
 module.exports = {
-    AuthLogin
+    AuthLogin,
+    Auth
 }
