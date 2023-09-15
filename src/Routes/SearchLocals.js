83d7deb593d3779be600d921b32363e221d39c31
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-09-14 19:56:49 -0600

    byRange

--- src/Routes/SearchLocals.js
+++ src/Routes/SearchLocals.js
@@ -3,7 +3,7 @@ const {searchLocals, searchByTags} = require('../Controllers/SearchLocals')
 
 const route = Router(); 
 
-route.get('/:Latitude/:Longitude/:kilometers', searchLocals);
+route.get('/byRange/:Latitude/:Longitude/:kilometers', searchLocals);
 
 route.get('/byTags/:Latitude/:Longitude/:kilometers/:tags', searchByTags);
 
