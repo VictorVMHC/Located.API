a371e91a54be909f27774d6b25b4e33cc51497ce
Enzoadrien <martinezquirozadrien89@gmail.com>
2023-09-20 21:12:48 -0600

    add  pagination  FoodView

--- src/Controllers/SearchLocals.js
+++ src/Controllers/SearchLocals.js
@@ -17,36 +17,52 @@ const searchLocals = async (req, res = Response ) =>{
     }
 }
     
-const searchByTags = async (req, res = Response ) =>{
-    try {
-    const {Latitude ,Longitude, kilometers, tags} = req.params;
+const searchByTags = async (req, res = Response) => {
+    const { Latitude, Longitude, kilometers, tags} = req.params;
+    const regexTags = new RegExp(tags.split(',').join('|'), 'i'); 
     const page = parseInt(req.query.page) || 1;
-    const perPage = parseInt(req.query.perPage) || 10;
+    const limit = parseInt(req.query.limit) || 10;
 
-    const regex = new RegExp(tags, 'i');
     const filteredLocals = searchLatitudeAndLongitude(Latitude, Longitude, kilometers);
-    
-        const totalPages = Math.ceil(totalCategories / perPage);
 
+    try {
         const locals = await Locals.find({
-            'location.latitude': filteredLocals.latitude,           
+            'location.latitude': filteredLocals.latitude,
             'location.longitude': filteredLocals.longitude,
             $or: [
-                { tags: regex },
-                { name: regex }
+                { tags: regexTags }, 
+                { name: regexTags }  
             ]
         })
-        .skip((page - 1) * perPage)
-        .limit(parseInt(perPage))
-        .exec();
-        
-        res.json({
-            current_page: page,
-            total_pages: totalPages,
-            results: locals,
+        .skip((page - 1) * limit) 
+        .limit(limit);
+
+        const totalLocals =  await Locals.find({
+            'location.latitude': filteredLocals.latitude,
+            'location.longitude': filteredLocals.longitude,
+            $or: [
+                { tags: regexTags }, 
+                { name: regexTags }  
+            ]
+        }).countDocuments();
+
+        if(!locals){
+            return res.status(404).json({
+                err: 'No locals were found '
+            });
+        }
+
+        console.log(totalLocals, limit);
+        return res.status(200).json({
+            locals,
+            page,
+            totalPages: Math.ceil(totalLocals / limit)
         });
+
     } catch (error) {
-        console.error(error);
+        return res.status(500).json({
+            err: 'An error occurred while getting the locals'
+        })
     }
 }
 
--- src/Routes/SearchLocals.js
+++ src/Routes/SearchLocals.js
@@ -5,6 +5,6 @@ const route = Router();
 
 route.get('/byRange/:Latitude/:Longitude/:kilometers', searchLocals);
 
-route.get('/byTags/:Latitude/:Longitude/:kilometers/:tags/:limit', searchByTags);
+route.get('/byTags/:Latitude/:Longitude/:kilometers/:tags', searchByTags);
 
 module.exports = route;
