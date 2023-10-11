c1acb69f4adca6f1d4bb890e677eeb63cf172733
Enzoadrien <martinezquirozadrien89@gmail.com>
2023-10-10 23:56:36 -0600

    changes to searchByTags from SearchLocals

--- src/Controllers/SearchLocals.js
+++ src/Controllers/SearchLocals.js
@@ -22,7 +22,8 @@ const searchLocals = async (req, res = Response ) =>{
 }
     
 const searchByTags = async (req, res = Response) => {
-    const { Latitude, Longitude, kilometers, tags, userId } = req.params;
+    const { Latitude, Longitude, kilometers, tags} = req.params;
+    const tokenDecoded = req.tokenDecoded;
     const regexTags = tags ? new RegExp(tags.split(',').join('|'), 'i') : /.*/; 
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
@@ -37,7 +38,7 @@ const searchByTags = async (req, res = Response) => {
         const localsWithLikes = [];
         for (const local of localsInRange) {
             const localLikes = await LikeLocal.countDocuments({localId: local._id })
-            const resultLikes = await LikeLocal.findOne({ userId: userId, localId: local._id }).select('_id');
+            const resultLikes = await LikeLocal.findOne({ userId: tokenDecoded.id, localId: local._id }).select('_id');
             const liked = resultLikes ? true : false;
             localsWithLikes.push({
                 ...local.toObject(),
--- src/Routes/SearchLocals.js
+++ src/Routes/SearchLocals.js
@@ -8,7 +8,12 @@ const route = Router();
 
 route.get('/byRange/:Latitude/:Longitude/:kilometers', searchLocals);
 
-route.get('/byTags/:Latitude/:Longitude/:kilometers/:tags/:userId', searchByTags);
+route.get('/byTags/:Latitude/:Longitude/:kilometers/:tags',[
+    check('x-token', 'Token is require').notEmpty(),
+    check('x-token', 'Token is not a JWT').isJWT(),
+    check('x-token', 'Token validation').custom(async (value, { req }) => await verifyToken(value, req)),
+    validationResults
+], searchByTags);
 
 route.get('/localsPopular/:Latitude/:Longitude/:kilometers',[
     check('x-token', 'Token is require').notEmpty(),
