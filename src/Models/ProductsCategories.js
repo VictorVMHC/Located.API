ad586eb5d698faded5bc2b5114a57896de915a47
Enzoadrien <martinezquirozadrien89@gmail.com>
2023-10-19 01:00:18 -0600

    delete productsCategories

--- src/Models/ProductsCategories.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const productsCategories = Schema({
-    category: {
-        type: String, 
-    }
-});
-
-module.exports = model('productsCategories', productsCategories );
